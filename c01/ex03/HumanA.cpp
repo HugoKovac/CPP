a7fe19809900c6cc0617637f2acf2fbb41bf8432
HugoKovac <97465389+HugoKovac@users.noreply.github.com>
2022-04-20 12:32:15 +0200

    Delete c01 directory

--- c01/ex03/HumanA.cpp
+++
@@ -1,8 +0,0 @@
-#include "HumanA.hpp"
-
-HumanA::HumanA(std::string str, Weapon *ptr) : _name(str), _Weapon(*ptr){
-}
-
-void HumanA::attack(void) const{
-	std::cout << this->_name << " attack with their " << this->_Weapon.getType() << std::endl;
-}
