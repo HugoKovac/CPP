f63679ab9a023c348e9627a9f70323328f751ba8
Hugo KOVAC <hkovac@e1r12p22.42.fr>
2022-05-16 14:46:25 +0200

    new main

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <list>
-#include "Mutantstack.cpp"
+#include "MutantStack.cpp"
 
 int main(void)
 {
@@ -16,14 +16,37 @@ int main(void)
 	std::cout << "size : " << test.size() << std::endl;
 	std::cout << "on top : " << test.top() << std::endl;
 	
+	std::cout <<"*MutantStack iterator*" << std::endl;
+
+	MutantStack<int>::iterator it = test.begin();
+	for (; it != test.end(); it++)
+		std::cout << *it << std::endl;
+
+	std::cout <<"*MutantStack reverse iterator*" << std::endl;
+
+	MutantStack<int>::reverse_iterator rit = test.rbegin();
+	for (; rit != test.rend(); rit++)
+		std::cout << *rit << std::endl;
+
 	std::cout << "-pop-" << std::endl;
 	test.pop();
 
 	std::cout << "size : " << test.size() << std::endl;
 	std::cout << "on top : " << test.top() << std::endl;
-
 	
-	std::cout <<"*List*" << std::endl;
+	std::cout <<"*MutantStack iterator*" << std::endl;
+
+	it = test.begin();
+	for (; it != test.end(); it++)
+		std::cout << *it << std::endl;
+
+	std::cout <<"*MutantStack reverse iterator*" << std::endl;
+
+	rit = test.rbegin();
+	for (; rit != test.rend(); rit++)
+		std::cout << *rit << std::endl;
+
+	std::cout <<"*std::list*" << std::endl;
 	std::list<int> test2;
 
 	std::cout <<"-push 1, 2, 3-" << std::endl;
@@ -33,12 +56,35 @@ int main(void)
 
 	std::cout << "size : " << test2.size() << std::endl;
 	std::cout << "on top : " << test2.back() << std::endl;
-	
+
+	std::cout <<"*std::list iterator*" << std::endl;
+
+	std::list<int>::iterator it2 = test2.begin();
+	for (; it2 != test2.end(); it2++)
+		std::cout << *it2 << std::endl;
+
+	std::cout <<"*std::list reverse iterator*" << std::endl;
+
+	std::list<int>::reverse_iterator rit2 = test2.rbegin();
+	for (; rit2 != test2.rend(); rit2++)
+		std::cout << *rit2 << std::endl;
+
 	std::cout << "-pop-" << std::endl;
 	test2.pop_back();
 
 	std::cout << "size : " << test2.size() << std::endl;
 	std::cout << "on top : " << test2.back() << std::endl;
 
-	return 0;
+	std::cout <<"*std::list iterator*" << std::endl;
+
+	it2 = test2.begin();
+	for (; it2 != test2.end(); it2++)
+		std::cout << *it2 << std::endl;
+
+	std::cout <<"*std::list reverse iterator*" << std::endl;
+
+	rit2 = test2.rbegin();
+	for (; rit2 != test2.rend(); rit2++)
+		std::cout << *rit2 << std::endl;
+	
 }
